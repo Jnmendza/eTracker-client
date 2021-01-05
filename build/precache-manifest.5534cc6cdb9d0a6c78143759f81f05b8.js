6928bb95c359e462fa325862d90b7b1cdc27470d
Jnmendza <jonatmendoza@gmail.com>
2021-01-04 19:20:13 -0800

    added build

---
+++ build/precache-manifest.5534cc6cdb9d0a6c78143759f81f05b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791144973b414f874114e3bc22f4de57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adff323a0ad6b0abcc9",
+    "url": "/static/css/main.170a6857.chunk.css"
+  },
+  {
+    "revision": "656665b1e7dcf22e4d7e",
+    "url": "/static/js/2.23acca9f.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.23acca9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adff323a0ad6b0abcc9",
+    "url": "/static/js/main.d130ec97.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  },
+  {
+    "revision": "81c65c9bf47d327961c741e9a7911444",
+    "url": "/static/media/image.81c65c9b.jpg"
+  }
+]);
